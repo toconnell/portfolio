881f1abcbac044c92a92b644563fe22307324a5c
Timothy J O'Connell <toconnell@tyrannybelle.com>
2018-05-23 17:41:24 -0400

    0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# portfolio
+# Portfolio!
 My Portfolio Website, https://toconnell.info - Flask API + AngularJS
+
+## About
+Built primarily to be _a part of_ my portfolio, this simple site is a blog-style AngularJS 1 app with two views (one for users, one for administrators) that uses a basic API to manage authentication, post retrieval, etc.
+
+The API is developed in Flask (python3).
+
+The project contains config files for deployment using NGINX + gunicorn.
+
+
+# Installation
+
+Some notes on installation intended for my personal use.
+
+## Depends
+```
+# apt-get install python3-setuptools
+# apt-get install python3-pip
+# pip3 install pymongo, ConfigParser, flask, flask-cors, gunicorn
+```
--- api.py
+++ api.py
@@ -4,12 +4,15 @@
 #	flask
 #
 
-from flask import Flask
+from flask import Flask, Response, redirect
+from flask_cors import CORS
 
 #
 #	std lib
 #
-
+from bson import json_util
+import json
+import ssl
 
 #
 #	private
@@ -22,17 +25,54 @@ import settings
 #	app starts here!
 #
 
-app = Flask(__name__)
+application = Flask(__name__)
+CORS(application)
 
-@app.route('/')
-def index():
-    return "Hello, World! Moved"
+#
+#	routes
+#
+
+# redirect for all posts shorthand
+@application.route('/posts', methods=['GET'])
+def all_posts():
+	return redirect('/posts/0', code=302)
+
+# retrieve a number of posts in reverse chron
+@application.route('/posts/<count>', methods=['GET'])
+def posts(count):
+	try:
+		count = int(count)
+	except:
+		return Response(response='In /posts/[count]; requests, [count] must be an integer!', status=400)
+
+	if count == 0:
+		posts = settings.mdb.posts.find({'published': True}).sort('created_on', -1)
+	else:
+		posts = settings.mdb.posts.find({'published': True}).sort('created_on', -1).limit(count)
+
+	return Response(
+		response=json.dumps(list(posts), default=json_util.default),
+		status=200,
+		mimetype="application/json"
+	)
+
+
+
+#
+#	__main__
+#
 
 if __name__ == '__main__':
-    app.run(
+	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+	context.load_cert_chain(
+		'/etc/letsencrypt/live/toconnell.info/fullchain.pem',
+		'/etc/letsencrypt/live/toconnell.info/privkey.pem',
+	)
+	application.run(
 		host=settings.get('api','host'),
 		port=settings.getint('api','port'),
-		debug=settings.getboolean('api','debug')
+		debug=settings.getboolean('api','debug'),
+		ssl_context=context
 	)
 
 
